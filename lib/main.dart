bfa9e578ba62aefa1766ee7b1bca6953e342afd7
azizhadiid <azizalhadiid88@gmail.com>
2026-09-16 20:09:02 +0700

    Setting Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/onboarding_page.dart';
 
-void main() {
+void main() async {
+  // 1. Pastikan binding flutter terinisialisasi
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Muat file .env untuk mengambil API Key & URL secara rahasia
+  await dotenv.load(fileName: ".env");
+
+  // 3. Inisialisasi koneksi Supabase
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    publishableKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+  );
+
   runApp(const MyApp());
 }
 
